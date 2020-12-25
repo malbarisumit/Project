037caf47252e3190776cdc0e8c4df7f5774cc951
malbarisumit <malbarisumit@gmail.com>
2020-12-25 09:53:38 +0530

    Network package

---
+++ Plasma_Donation_System/src/Business/Network/Network.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Network;
+
+import Business.Enterprise.EnterpriseDirectory;
+
+/**
+ *
+ * @author MyPC1
+ */
+public class Network {
+    private String name;
+    private String state;
+    private String country;
+    
+    
+    
+    private EnterpriseDirectory enterpriseDirectory;
+    
+    public Network(){
+        enterpriseDirectory=new EnterpriseDirectory();
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public EnterpriseDirectory getEnterpriseDirectory() {
+        return enterpriseDirectory;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    
+    
+    
+    @Override
+    public String toString(){
+        return name;
+    }
+    
+}
